25c1d29ea167a3af78231955fd46f7a8e9247966
SaxophOnyx <kysbeg@gmail.com>
2022-05-30 10:26:04 +0300

    Init

---
+++ FileStorageLab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FileStorageLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8005/";
+            string filePath = "D://files//";
+
+            HttpFilesHandler server = new HttpFilesHandler(url, filePath);
+            server.Start();
+
+            Console.WriteLine("Press any key to stop...");
+            Console.ReadKey();
+            server.Stop();
+        }
+    }
+}
